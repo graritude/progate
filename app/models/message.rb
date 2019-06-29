3b25bff653772e002d83b38e32773f5b605f3c4d
EC2 Default User <ec2-user@ip-172-31-38-203.us-east-2.compute.internal>
2019-06-29 05:13:28 +0000

    first commit

---
+++ app/models/message.rb
@@ -0,0 +1,9 @@
+class Message < ApplicationRecord
+  # before_action :authenticate_user!, :only => [:create]
+
+  def create
+    # @message = Message.create(params.require(:message).permit(:user_id, :receiver, :content).merge(:user_id => current_user.id))
+    # redirect_to :back
+  end
+      
+end
